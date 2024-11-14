2b4981a141d57753f346b1d7f461d1177e74252d
Ratna Kadam <123215740+RatnaKadam@users.noreply.github.com>
2024-11-14 13:45:14 +0100

    Added Website_using_bootstrap

--- django_project/website_using_bootstrap/new_app/views.py
+++ django_project/website_using_bootstrap/new_app/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def hello_view(request):
-    my_dict = {'insert_me':"Hello from views!!"}
-    return render(request, 'index.html', context=my_dict)
+    #my_dict = {'insert_me':"Hello from views!!"}
+    return render(request, 'index.html')
+    #return render(request, 'index.html', context=my_dict)
 
 def blog_view(request):
     return render(request, 'blog.html')
