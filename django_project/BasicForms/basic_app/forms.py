42cecb0fa16361a32afa22be0435f460f6cdfae2
Ratnaprabha Kadam <ratnaprabha_k@yahoo.com>
2024-11-19 17:07:57 +0100

    Renamed directories

--- django_project/BasicForms/basic_app/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from basic_app.models import UserData
-
-class FormName(forms.ModelForm):
-    class Meta:
-        model = UserData  # Link the form to the UserData model
-        fields = ['name', 'email', 'text']
